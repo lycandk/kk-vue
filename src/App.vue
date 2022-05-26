3fddc30cc4e860078615761681cf6137c4c8ca28
Makkapakka <1961605387@qq.com>
2022-05-26 14:58:50 +0800

    前端登录页面引入elementUI 使用form表单 并完善页面背景

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+<!--    <img src="./assets/logo.png">-->
     <router-view/>
   </div>
 </template>
@@ -14,12 +14,19 @@ export default {
 </script>
 
 <style>
+/*在App.vue中将样式中默认的margin-top 改为0，使得页面顶部不留白*/
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+}
+/* 在App.vue中加入body样式，使得背景图片四周不会留有空白*/
+body{
+  margin:0;
+  padding:0;
+  border:0
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入ElementUI
+import ElementUI from 'element-ui'
+// 引入CSS
+import 'element-ui/lib/theme-chalk/index.css'
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
-
 Vue.config.productionTip = false
+// 使用ElementUI
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
